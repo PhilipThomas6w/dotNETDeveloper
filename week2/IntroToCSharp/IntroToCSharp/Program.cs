07f94fac210343cb0a7184bfa327b977ef259207
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-03-27 16:15:11 +0100

    Updated IntroToCSharp.

--- week2/IntroToCSharp/IntroToCSharp/Program.cs
+++ week2/IntroToCSharp/IntroToCSharp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace IntroToCSharp;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+
+        int x = 100;
+        x += 10;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("i is: " + i + " , x is: " + x);
+            x = x + i;
+        }
+        Console.Read();
+    }
+}
+
+
