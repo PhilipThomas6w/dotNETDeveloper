e5813a03a32400d9e0fc67cffd77f26fdc16e2fb
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-03-28 11:01:50 +0100

    Added UnitTestLessonApp project.

---
+++ week2/UnitTestLessonApp/UnitTestLessonApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace UnitTestLessonApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int inputTimeOfDay = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMessage(inputTimeOfDay));
+
+        }
+
+        private static string GetMessage(int timeOfDay)
+        {
+            if (timeOfDay >= 5 && timeOfDay < 11)
+            {
+                return "Good morning!";
+            }
+            else if (timeOfDay >= 12 && timeOfDay < 18)
+            {
+                return "Good afternoon!";
+            }
+            else
+            {
+                return "Good evening!";
+            }
+        }
+    }
+}
