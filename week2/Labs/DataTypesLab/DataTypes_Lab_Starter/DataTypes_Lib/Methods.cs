e6abc367c87bad3ae3122872810c4385a546e774
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-03-30 14:41:08 +0100

    Updated DataTypesLab.

---
+++ week2/Labs/DataTypesLab/DataTypes_Lab_Starter/DataTypes_Lib/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataTypes_Lib
+{
+    public static class Methods
+    {
+        // write a method to return the product of all numbers from 1 to n inclusive
+        public static int Factorial(int n)
+        {
+            if (n < 1)
+            {
+                throw new ArgumentException("n must be a positive integer!");
+            }
+
+            int product = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                product *= i;
+            }
+
+            return product;
+        }
+
+        public static float Mult(float num1, float num2)
+        {
+            float result = num1 * num2;
+            return (float)Math.Round(result, 5);
+        }
+    }
+}
