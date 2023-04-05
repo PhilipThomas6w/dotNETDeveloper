7454df9b2229534a1c8924883b8375278b31ae66
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-04-06 00:35:36 +0100

    Restarted FourPillarsApp due to critical error.

---
+++ week3/Object Oriented Programming/Four Pillars/FourPillars/FourPillarsApp/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace FourPillarsApp;
+
+public class Person
+{
+    private string _firstName;
+    private string _lastName;
+
+    public int Age { get; set; }
+
+    public Person(string fName, string lName) // This is a *constructor*. Note, 1) Same name as class 2) No return type.
+    {
+        _firstName = fName;                     // This is a field - note the naming convention.
+        _lastName = lName;
+    }
+
+    public string GetFullName()
+    {
+        return $"{_firstName} + {_lastName}";
+    }
+
+
+    
+
+
+}
