e98d3def22a0e9bd71e5a43c862726cb6894a3dc
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-04-06 01:37:06 +0100

    Added Person class to FourPillarsApp.

--- week3/Object Oriented Programming/Four Pillars/FourPillars/FourPillarsApp/Person.cs
+++ week3/Object Oriented Programming/Four Pillars/FourPillars/FourPillarsApp/Person.cs
@@ -11,21 +11,34 @@ public class Person
     private string _firstName;
     private string _lastName;
 
-    public int Age { get; set; }
+    public int Age { get; set; }                // This is a property (note the {get; set; } feature)
 
-    public Person(string fName, string lName) // This is a *constructor*. Note, 1) Same name as class 2) No return type.
+    public Person(string fName, string lName)   // This is a *constructor*. Note, 1) Same name as class 2) No return type.
     {
-        _firstName = fName;                     // This is a field - note the naming convention.
+        _firstName = fName;                     // This is a field - note the _namingConvention
         _lastName = lName;
     }
 
-    public string GetFullName()
+    public Person(string fName, string lName, int age) : this(fName, lName)
     {
-        return $"{_firstName} + {_lastName}";
+        _firstName = fName;                     
+        _lastName = lName;
+        Age = age;
     }
 
 
-    
+    public string GetFullName()
+    {
+        return $"{_firstName} {_lastName}";
+    }
+
+    public string GetFullName(string title)
+    {
+        //return $"{title} {_firstName} {_lastName}";
+        return $"{title} {GetFullName()}";
+    }
+
+
 
 
 }
--- week3/Object Oriented Programming/Four Pillars/FourPillars/FourPillarsApp/Program.cs
+++ week3/Object Oriented Programming/Four Pillars/FourPillars/FourPillarsApp/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace FourPillarsApp
+﻿namespace FourPillarsApp;
+
+public class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        // instantiation
+        Person andrew = new Person("Andrew", "Ma"); // Here, <new> is the constructor we use to instantiate an object, <andrew>, belonging to the class <Person>
+        andrew.Age = 22;                            // To assign a value to any member of a class, we use dot-notation.
+
+        /*andrew._firstName = "Andy";*/             // We can't update the _firstName field because it's protection level is set to private i.e., it has been encapsulated within the Person class.
+
+        Console.WriteLine(andrew.GetFullName());    // You have to call the method on an object, since it's objects that actually store data. You can't call them on a class itself.
+
+        var talal = new Person("Talal", "Hassan", 22);
+        Console.WriteLine(talal.GetFullName("Mr"));
+
+
+
     }
 }
