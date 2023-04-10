54f59613fcd51ea3fc41073bf95e147b697a2ecf
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-04-10 22:45:56 +0100

    Reviewed Week3 OOP code

---
+++ week3/03. Object Oriented Programming/FourPillarsApp/FourPillarsTests/ClassStructsTests.cs
@@ -0,0 +1,97 @@
+using FourPillars2App;
+
+namespace FourPillarsTests;
+
+public class ClassStructsTests
+{
+    Person _sut;
+
+    [Test]
+    [Category("Constructor Tests")]
+    public void GivenNoArguments_Person_IsCreated()
+    {
+        // Act
+        _sut = new Person();
+
+        //Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    [Category("Constructor Tests")]
+    public void GivenFirstNameLastName_Person_IsCreated()
+    {
+        // Act
+        _sut = new Person("John", "Smith");
+
+        //Assert
+        Assert.Pass();
+    }
+
+    [Test]
+    [Category("Constructor Tests")]
+    public void GivenFirstNameLastNameAge_Person_IsCreatedWithAge()
+    {
+        // Act
+        _sut = new Person("John", "Smith", 34);
+
+        //Assert
+        Assert.That(_sut.Age, Is.EqualTo(34));
+    }
+
+    [Test]
+    [Category("Constructor Tests")]
+    public void GivenFirstNameLastNameAgeHeight_Person_IsCreatedWithAgeAndHeight()
+    {
+        // Act
+        _sut = new Person("John", "Smith", 34, 180);
+
+        //Assert
+        Assert.That(_sut.Height, Is.EqualTo(180));
+    }
+
+    [Test]
+    [Category("Method Tests")]
+    public void GivenConstructor1_GetFullName_ReturnsCorrectFullName()
+    {
+        // Arrange
+        Person testPerson = new Person("John", "Smith");
+
+        // Act
+        string actualResult = "John Smith";
+
+        //Assert
+        Assert.That(actualResult, Is.EqualTo(testPerson.GetFullName()));
+    }
+
+    [Test]
+    [Category("Method Tests")]
+    public void GivenConstructor1_GetFullName_ReturnsTitleFullName()
+    {
+        // Arrange
+        Person testPerson = new Person("John", "Smith");
+
+        // Act
+        string actualResult = "Mr John Smith";
+
+        //Assert
+        Assert.That(actualResult, Is.EqualTo(testPerson.GetFullName("Mr")));
+    }
+
+    [Test]
+    [Category("Property Tests")]
+    public void GivenSetAge_Get_ReturnsAge()
+    {
+        // Arrange
+        Person testPerson = new Person("John", "Smith");
+        testPerson.Age = 34;
+
+        // Act
+        var _age = testPerson.Age;
+        
+
+        //Assert
+        Assert.That(_age, Is.EqualTo(testPerson.Age));
+    }
+
+}
