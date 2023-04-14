588ef979e97992de355327e787c1824e3b3f7c18
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-04-14 10:49:35 +0100

    Added Northwind Database

---
+++ week4/02. Refactoring/Code Smells/SafariParkCodeSmells_Starter/SafariPark/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesApp
+{
+    class Program
+    {
+        static void SpartaWrite(Object obj)
+        {
+            Console.WriteLine(obj.ToString());
+            if (obj is Hunter)
+            {
+                var hunterObj = (Hunter)obj;
+                Console.WriteLine(hunterObj.Shoot());
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Person("Katie", "King");
+            Hunter h = new Hunter("Marion", "Jones", new Camera("Leica")) { Age = 32 };
+
+            Console.WriteLine();
+            var theWeapons = new List<IShootable>();
+            theWeapons.Add(new Weapon(WeaponType.LaserGun, "ToysRUs"));
+            theWeapons.Add(new Weapon(WeaponType.Waterpistol, "Supersoaker"));
+            theWeapons.Add(new Weapon(WeaponType.LaserGun, "ZippaZap"));
+            theWeapons.Add(new Hunter("Cathy", "French", new Camera("Minolta")));
+            theWeapons.Add(new Camera("Pentax"));
+            foreach (var w in theWeapons)
+            {
+                Console.WriteLine(w.Shoot());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Polymorphic shootout");
+            Camera pentax = new Camera("Pentax");
+            var pistol = new Weapon(WeaponType.Waterpistol, "Supersoaker");
+            var laserGun = new Weapon(WeaponType.LaserGun, "ZippaZap");
+            Hunter nish = new Hunter("Nish", "Mandal", pentax);
+            Console.WriteLine(nish.Shoot());
+            nish.Shooter = pistol;
+            Console.WriteLine(nish.Shoot());
+            nish.Shooter = laserGun;
+            Console.WriteLine(nish.Shoot());
+            nish.Shooter = pistol;
+            Console.WriteLine(nish.Shoot());
+
+        }
+
+    }
+}
---
+++ week4/03. Design Patterns/DesignPatternExamples/SingletonPattern/Singleton.cs
@@ -0,0 +1,43 @@
+﻿namespace SingletonPattern
+{
+    //This example is NOT thread safe
+    public class Singleton 
+    {
+        private Singleton() { }
+        //Ensures thread safety
+        private static readonly object syncObj = new Object();
+
+        private int _counter = 0;
+
+        private static Singleton? _instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                // Check if syncObj has been created before creating the instance
+                // Ensures one thread 
+                lock (syncObj)
+                {
+                    if (_instance == null)
+                        _instance = new Singleton();
+                }
+                return _instance;
+
+            }
+        }
+        public int Increment()
+        {
+            return _counter++;
+        }
+        public string GetCount()
+        {
+            return "Counter Value: " + _counter.ToString();
+        }
+
+        public void LogException(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
