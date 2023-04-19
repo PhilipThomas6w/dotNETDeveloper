a942165b6f0637b9818b6d876c703d30869e7feb
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-04-19 23:06:36 +0100

    Completed CRUD program

---
+++ week5/01. SQL with C#/SQL_CSharp/SQL_CSharp/Customer.cs
@@ -0,0 +1,21 @@
+﻿namespace SQL_CSharp;
+
+public class Customer
+{
+    public string CustomerID { get; set; }
+    public string CompanyName { get; set; }
+    public string ContactName { get; set; }
+    public string ContactTitle { get; set; }
+    public string City { get; set; }
+    public string? Region { get; set; }
+
+    public string GetFullName()
+    {
+        return $"{ContactTitle} - {ContactName}";
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $"{CustomerID} {CompanyName} {ContactName} {ContactTitle} {City} {Region}";
+    }
+}
